fa354ff2140ff2d67a548e65cfa95c374a3bbedc
Lantop <41171274+Lantop1k@users.noreply.github.com>
2025-02-21 23:04:07 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,12 @@
-import { render, screen } from "@testing-library/react";
+// Updated App.test.js
+import { render } from "@testing-library/react";
 import App from "./App";
 
-beforeAll(() => {
-  if (!global.navigator.mediaDevices) {
-    global.navigator.mediaDevices = {};
-  }
-
-  global.navigator.mediaDevices.getUserMedia = jest.fn().mockResolvedValue({
-    getTracks: () => [],
-  });
-});
+global.navigator.mediaDevices = {
+  getUserMedia: jest.fn().mockResolvedValue({ getTracks: () => [] }),
+};
 
 test("renders without crashing", async () => {
   render(<App />);
-  
-  // Wait for the effect to resolve
-  await expect(global.navigator.mediaDevices.getUserMedia).toHaveBeenCalled();
+  expect(global.navigator.mediaDevices.getUserMedia).toHaveBeenCalled();
 });
